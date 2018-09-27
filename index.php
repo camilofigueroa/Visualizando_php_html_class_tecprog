9defa12f2638629243229d2023336752c120b697
Edgar Camilo Figueroa Rivera <figueroacamilo@gmail.com>
2018-09-27 17:00:41 -0500

    Ajustes en una tabla html

--- index.php
+++ index.php
@@ -15,11 +15,17 @@
             include( "clases/Visualizando.php" );
             $obj_visual = new Visualizando();
             
+
+            //Esta parte de la conexión no debería estar aquí pero es un ejercicio
             $conexion = mysqli_connect( "localhost", "root", "", "bd_prueba" );
             $sql = "SELECT * FROM tb_usuarios";
             $resultado = $conexion->query( $sql );
+            
+            //La siguiente línea pinta los resultados de la consulta.
+            //echo $obj_visual->retornar_vista_consulta( $resultado );
 
-            echo $obj_visual->retornar_vista_consulta( $resultado );
+            //La siguiente línea pinta los resultados de la consulta en una tabla HTML.
+            echo $obj_visual->retornar_vista_consulta( $resultado, 1 );
 
         ?>
 
