7d74c5bf42c5ce10aedda640010c2d100c0c8440
Edgar Camilo Figueroa Rivera <figueroacamilo@gmail.com>
2018-09-27 17:28:25 -0500

    Actualizando la vista
    En esta parte se trabajan dos clases, una para el modelo y la otra para el controlador. Es onvio que los archivos de la vista deben acoplarse a estas nuevas modificaciones.

--- index.php
+++ index.php
@@ -13,19 +13,18 @@
         <?php
 
             include( "clases/Visualizando.php" );
-            $obj_visual = new Visualizando();
+            $obj_visual = new Visualizando(); //Visualizando puede usar los métodos de BD por herencia.
             
+            //Se elimina la parte de la base de datos porque de eso se va a encargar otra clase.
 
-            //Esta parte de la conexión no debería estar aquí pero es un ejercicio
-            $conexion = mysqli_connect( "localhost", "root", "", "bd_prueba" );
-            $sql = "SELECT * FROM tb_usuarios";
-            $resultado = $conexion->query( $sql );
-            
-            //La siguiente línea pinta los resultados de la consulta.
-            //echo $obj_visual->retornar_vista_consulta( $resultado );
+            //Resultado vuelve a tomar todo lo de la tabla resultados, pero mejor, desde una clase.
+            //Todo ese código queda encapsulado, es decir, escondido y no hiere la retina.
+            $resultado = $obj_visual->procesar_info_tabla( "tb_usuarios" );
 
             //La siguiente línea pinta los resultados de la consulta en una tabla HTML.
             echo $obj_visual->retornar_vista_consulta( $resultado, 1 );
+
+            //---------en teoría... ya se podría usar con cualquier tabla.
 
         ?>
 
